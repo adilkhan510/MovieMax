d485e2788cedd0ed4bb76c46a2df703af026da6f
adilkhan <adilkhan@ucsb.edu>
2020-05-02 17:22:13 -0700

    setting up env file

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : "mongodb+srv://adilkhan:nicepassword@movie-app-pdiud.mongodb.net/test?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -2,15 +2,21 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const config = require('./config/key')
 
-
-mongoose.connect('mongodb+srv://adilkhan:nicepassword@movie-app-pdiud.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParse: true}).then(()=>{
+// -----Connect to the database
+mongoose.connect(config.mongoURI,{useNewUrlParse: true}).then(()=>{
     console.log("Database connected")
 }).catch((error)=>{
     console.log(error)
 })
 
+// ----setting up middleware 
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
-
-// Setting up server to listen on port 5000
+// ---- Setting up server to listen on port 5000
 app.listen(5000)
