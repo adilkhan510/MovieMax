550eafa4686efa5eda8894a0b0e09a83cc1b2ba2
adilkhan <adilkhan@ucsb.edu>
2020-05-02 23:24:35 -0700

    got some more stuff done

--- server/model/user.js
+++ server/model/user.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+
+const saltRounds = 10
 const userSchema = mongoose.Schema({
     firstName : {
         type : String,
-        maxLength = 50,
+        maxLength : 50,
     },
     email : {
         type : String,
@@ -23,10 +26,22 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre('save', ( next )=>{
     let user = this;
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds,(err,salt)=>{
+            if(err) return next(err)
+
+            bcrypt.hash(user.password,salt,(err,hash)=>{
+                if(err) return next(err);
+                user.password = hash
+            })
+        })
+    }else{
+        next()
+    }
 })
 
 
-const User = mongoose.Model("User", userSchema)
+const User = mongoose.model("User", userSchema)
 
 module.exports = {
     User
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const config = require('./config/key')
+const { User } = require('./model/user')
 
 // -----Connect to the database
 mongoose.connect(config.mongoURI,{useNewUrlParse: true}).then(()=>{
@@ -17,6 +18,21 @@ mongoose.connect(config.mongoURI,{useNewUrlParse: true}).then(()=>{
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
 app.use(cookieParser())
+
+app.post('/api/users/register',(req,res)=>{
+    const user = new User(req.body);
+    user.save((err,userData)=>{
+        if(err) return res.json({
+        success : false,
+        err
+        })
+        res.status(200).json({
+            success : true,
+            data : doc
+        })
+    })
+
+})
 
 // ---- Setting up server to listen on port 5000
 app.listen(5000)
