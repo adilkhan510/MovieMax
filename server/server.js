da3a892fcc333173c957b6af0f76cc166626b38b
adilkhan <adilkhan@ucsb.edu>
2020-06-18 00:05:42 -0700

    fixing bugs

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.use(express.static("client/build"));
 
 // index.html for all page routes
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname + '/app/client/build/index.html'));
 });
 }
 
