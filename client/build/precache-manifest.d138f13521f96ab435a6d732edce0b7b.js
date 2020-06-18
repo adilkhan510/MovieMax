d71fd7147465f798adb7f94cc4dcd91c9048c2a0
adilkhan <adilkhan@ucsb.edu>
2020-06-17 23:44:46 -0700

    removed build

---
+++ client/build/precache-manifest.d138f13521f96ab435a6d732edce0b7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577012a5fa0e37d9cc659b788a21b7fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59bbf93950abb2d8ef62",
+    "url": "/static/css/main.a05b5eae.chunk.css"
+  },
+  {
+    "revision": "87e929da8b58ff1afbe4",
+    "url": "/static/js/2.07b654c7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.07b654c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59bbf93950abb2d8ef62",
+    "url": "/static/js/main.09eded66.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c110bf9447b11ac3fff9d8cc39c3c4ee",
+    "url": "/static/media/logo.c110bf94.png"
+  }
+]);
