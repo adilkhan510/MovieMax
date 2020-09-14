0b117758e8ce30bb83b27c3e82c6c47b0e884b47
Adil Khan <54783965+Adilkhan510@users.noreply.github.com>
2020-09-13 22:08:28 -0700

    Update MovieCard.js
    update

--- client/src/components/Home/MovieCard.js
+++ client/src/components/Home/MovieCard.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Paper } from '@material-ui/core'
 
+
+
 export const MovieCard = (props) => {
     if (props.mainPage) {
         return (
             <a href={`/movie/${props.id}`}>
-                <img src={props.movieUrl} style={{ width: "80%", height: "400px" }} alt="movie" />
+                <img src={props.movieUrl} style={{
+                    width: "80%", height: "400px"
+
+                }} alt="movie" />
             </a>
         )
     } else {
